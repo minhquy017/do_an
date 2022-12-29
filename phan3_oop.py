553eb0dedde69b4da0890a6e56a7e6caf31bb68b
minhquy <115064839+minhquy017@users.noreply.github.com>
2022-12-29 19:08:48 +0700

    phần 3 lập trình hướng đối tượng
    phần 3 lập trình hướng đối tượng

---
+++ phan3_oop.py
@@ -0,0 +1,14 @@
+class NhanVien:
+
+    def __init__(self,hoten,tuoi,luong):
+        self.hoten=hoten
+        self.tuoi=tuoi
+        self.luong=luong
+    def __str__(self):
+        message = "[hoten: " + self.hoten + "; tuoi:" + str(self.tuoi) + "; luong: " + str(self.luong) + "]"
+        return message
+
+
+
+
+
